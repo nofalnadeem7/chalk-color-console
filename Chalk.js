d480be341c895c2a8fb679d62aeff8374d1e4096
Nofal Nadeem <nofalnadeem@192.168.1.15>
2025-07-06 04:20:51 +0500

    Tried chalk module in JS

---
+++ Chalk.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("This text is blue"));
+console.log(chalk.red("This text is red"));
+console.log(chalk.yellow("This text is yellow"));
+console.log(chalk.green("This text is green"));
+
+console.log("------------------------");
+
+console.log(chalk.bold("This text is bold"));
+console.log(chalk.underline("This text is underline"));
+console.log(chalk.bgCyanBright("This text has a CyanBright background"));
